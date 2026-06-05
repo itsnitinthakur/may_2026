e44fcad39bae3313a3ea6918e535e53cc8cce309
itsnitinthakur <41230053+itsnitinthakur@users.noreply.github.com>
2026-06-05 06:13:00 -0700

    adding ibm practice questions

---
+++ code_test.py
@@ -0,0 +1,15 @@
+n=15
+answer = []
+
+
+for i in range(n):
+    if (i+1)%3 == 0 and (i+1)%5 == 0:
+        answer.append("FizzBuzz")
+    elif (i+1)%3 == 0:
+        answer.append("Fizz")
+    elif (i+1)%5 == 0:
+        answer.append("Buzz")
+    else:
+        answer.append(str(i+1))
+
+print(answer)
